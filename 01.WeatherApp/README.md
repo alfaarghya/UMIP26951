7e786c8228119907d0d95eb216ee8c0b36124f1a
alfaarghya <alfaarghya.dev@gmail.com>
2025-01-08 19:51:00 +0530

    basic app setup 🚀

---
+++ 01.WeatherApp/README.md
@@ -0,0 +1,11 @@
+## Weather App ⛅
+
+### run the site locally 🏃🏽‍♂️
+
+```bash
+
+yarn install # install require package
+
+yarn run dev # start the application
+
+```
---
+++ 01.WeatherApp/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-4xl text-center bg-blue-300">
+      <h1>Weather App ⛅</h1>
+    </div>
+  )
+}
+
+export default App;
