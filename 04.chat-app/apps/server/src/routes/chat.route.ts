e075edf2601e34788522c654913122d6aefdec4f
alfaarghya <alfaarghya.dev@gmail.com>
2025-03-20 12:08:30 +0530

    chat-app: retrieve the message history

--- 04.chat-app/apps/server/src/routes/chat.route.ts
+++ 04.chat-app/apps/server/src/routes/chat.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate";
-import { getUserChats } from "../controller/chatController";
+import { getMessages, getUserChats } from "../controller/chatController";
 
 
 const router = Router();
 
 //get the list of rooms and inboxes
 router.get("/lists", authenticate, getUserChats);
+
+//get the message history of a room or inbox
+router.get("/:roomOrInboxId", authenticate, getMessages);
 
 
 export default router;
--- 04.chat-app/packages/types/src/serverTypes.ts
+++ 04.chat-app/packages/types/src/serverTypes.ts
@@ -18,3 +18,12 @@ export const UserSignInSchema = z.object({
 export const GetUserChatsSchema = z.object({
   userId: z.string().min(1, "User ID is required"),
 });
+
+// Validation for retrieving messages
+export const GetMessagesSchema = z.object({
+  userId: z.string().uuid(),
+  inboxId: z.string().uuid().optional(), // Required for direct messages
+  roomId: z.string().uuid().optional(), // Required for room messages
+}).refine((data) => data.userId || data.roomId, {
+  message: "Must provide either userId (for direct messages) or roomId (for room messages)",
+});
