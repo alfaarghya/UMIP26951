fb40403bc91006e5bc77fa1fc8080afb386af1fb
alfaarghya <alfaarghya.dev@gmail.com>
2025-03-20 11:59:52 +0530

    chat-app: encrypt & decrypt message

---
+++ 04.chat-app/apps/server/src/utils/encryption.ts
@@ -0,0 +1,18 @@
+import crypto from "crypto";
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const SECRET_KEY = process.env.SECRET_KEY || "default_secret";
+
+export const encryptMessage = (message: string): string => {
+  const cipher = crypto.createCipheriv("aes-256-cbc", SECRET_KEY, null);
+  let encrypted = cipher.update(message, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return encrypted;
+};
+
+export const decryptMessage = (encryptedMessage: string): string => {
+  const decipher = crypto.createDecipheriv("aes-256-cbc", SECRET_KEY, null);
+  let decrypted = decipher.update(encryptedMessage, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+};
