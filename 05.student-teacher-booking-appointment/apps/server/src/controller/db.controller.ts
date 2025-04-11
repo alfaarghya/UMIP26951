37a1d5790624a270500fbfc194e6c2641d6a1bd5
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-11 16:38:05 +0530

    stba: backend server boilerplate code

---
+++ 05.student-teacher-booking-appointment/apps/server/src/controller/db.controller.ts
@@ -0,0 +1,13 @@
+import prisma from "@stba/db/prisma";
+
+const dbConnect = async () => {
+  try {
+    await prisma.$connect();
+    console.log("✅ Database connected successfully!");
+  } catch (err) {
+    console.error("❌ Error connecting to the database:", err);
+    process.exit(1); // Exit if DB connection fails
+  }
+};
+
+export default dbConnect;
---
+++ 05.student-teacher-booking-appointment/apps/server/src/index.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+import * as dotenv from "dotenv";
+import startServer from "./controller/start.controller";
+
+dotenv.config();
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const port = process.env.PORT || 8080; // Server port
+
+// Start the server (Express & WebSocket)
+app.listen(port, async () => startServer(Number(port)));
