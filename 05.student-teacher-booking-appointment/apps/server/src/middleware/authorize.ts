ec39a0cdb63812651789363e82fe2c07b92c1bb3
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-13 15:37:23 +0530

    sbta: middleware -> student access only

--- 05.student-teacher-booking-appointment/apps/server/src/middleware/authorize.ts
+++ 05.student-teacher-booking-appointment/apps/server/src/middleware/authorize.ts
@@ -28,3 +28,28 @@ export const authorizeAdmin = (req: Request, res: Response, next: NextFunction) 
   }
 };
 
+export const authorizeStudent = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    //get the role form body
+    const { role } = req.body;
+
+    //only students allowed
+    if (role !== "STUDENT") {
+      return res.status(Status.Forbidden).json({
+        status: Status.Forbidden,
+        statusMessage: StatusMessages[Status.Forbidden],
+        message: "Access denied. Students only",
+      });
+    }
+
+    next();
+  } catch (err) {
+    console.error("JWT verification failed:", err);
+    res.status(Status.InternalServerError).json({
+      status: Status.InternalServerError,
+      statusMessage: StatusMessages[Status.InternalServerError],
+      message: "Internal server error Please try again later.",
+    });
+    return;
+  }
+};
