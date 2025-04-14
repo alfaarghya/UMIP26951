64821c0942d4b62176055f7115b56341f937c170
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-14 19:12:43 +0530

    sbta: connect all routes

--- 05.student-teacher-booking-appointment/apps/server/src/app.ts
+++ 05.student-teacher-booking-appointment/apps/server/src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import checkRoutes from "./middleware/checkRoutes";
+import authRoutes from "./routes/auth.route";
+import adminRoutes from "./routes/admin.route";
+import studentRoutes from "./routes/student.route";
+import teacherRoutes from "./routes/teacher.route";
 
 const app = express();
 
@@ -11,6 +15,10 @@ app.use(express.json());
 app.use(cookieParser()); //parse the cookies
 app.use(checkRoutes); // check all routes 
 
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/teacher", teacherRoutes);
 
 // Default Route
 app.get("/", (_req, res) => {
