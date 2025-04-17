d00a09aa9040d7921bc2fd95241669719aef4f30
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-17 15:44:41 +0530

    stba: update components

--- 05.student-teacher-booking-appointment/packages/ui/src/Hero.tsx
+++ 05.student-teacher-booking-appointment/packages/ui/src/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   const role = typeof window !== "undefined" ? localStorage.getItem("role") || "" : "";
 
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center text-center py-20 pt-30 bg-gradient-to-b from-blue-500 to-white">
+    <section className="w-full h-full flex flex-col justify-center items-center text-center py-20 bg-gradient-to-b from-blue-500 to-white">
       <div className="m-5">
         <h2 className="text-4xl font-bold text-blue-700 mb-4">Book Appointments Easily</h2>
         <p className="text-lg text-gray-700 max-w-xl mx-auto">
@@ -15,7 +15,7 @@ const Hero = () => {
         </p>
       </div>
       <button
-        onClick={() => role === "" ? router.push("/signin") : router.push("/dashboard")}
+        onClick={() => role === "" ? router.push("/login") : router.push("/dashboard")}
         className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-3 rounded-lg text-lg shadow-md transition duration-300"
       >
         Get Started
--- 05.student-teacher-booking-appointment/packages/ui/src/Navbar.tsx
+++ 05.student-teacher-booking-appointment/packages/ui/src/Navbar.tsx
@@ -1,20 +1,51 @@
 'use client'
 
 import Link from 'next/link'
+import { useRouter } from "next/navigation";
 import { NavBarProps } from "@stba/types/client"
+import { toast } from 'sonner';
+import { useUser } from './context/UserContext';
 
 const Navbar = ({ className, appName }: NavBarProps) => {
+  const router = useRouter();
+  const { name, setName } = useUser();
+
+  const logout = () => {
+    localStorage.removeItem('name');
+    localStorage.removeItem('role');
+    document.cookie = 'token=; Max-Age=0; path=/';
+    toast.success('Logged out successfully');
+    setName('');
+    router.push('/login');
+  };
+
   return (
     <nav className="w-full px-6 py-4 bg-gray-800 shadow-md flex justify-between items-center fixed">
       <div>
-        <h1 className="text-4xl font-bold text-blue-500">STBA</h1>
-        <p className={`text-sm text-center ${className}`}>{appName}</p>
+        <Link href="/" className="">
+          <h1 className="text-4xl font-bold text-blue-500">STBA</h1>
+          <p className={`text-sm text-center ${className}`}>{appName}</p>
+        </Link>
       </div>
-      <Link href="/signin">
-        <button className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-lg text-black  transition">
-          Sign In
-        </button>
-      </Link>
+      <div>
+        {name === "" ? (
+          <Link href="/login">
+            <button className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-lg text-black  transition">
+              Log In
+            </button>
+          </Link>
+        ) : (
+          <div className="flex items-center gap-4">
+            <span className="text-gray-200">Hello, {name}</span>
+            <button
+              onClick={logout}
+              className="bg-red-600 hover:bg-red-700 px-5 py-2 rounded-lg text-lg transition duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
