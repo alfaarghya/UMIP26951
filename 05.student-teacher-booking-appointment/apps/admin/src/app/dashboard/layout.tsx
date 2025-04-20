d3f8880914c5599eef56d648b79e8ea8f7db2ee7
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-20 17:00:58 +0530

    stba: add dashboard to all sites

---
+++ 05.student-teacher-booking-appointment/apps/admin/src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from '@stba/ui/Sidebar';
+import { Role } from '@stba/types/client';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex w-full h-screen">
+      <Sidebar role={Role.ADMIN} />
+      <main className="flex-1 overflow-y-auto bg-white p-6">{children}</main>
+    </div>
+  );
+}
