be7c9542c4278ba10d785b3f76f8badff6c1d079
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-20 18:50:47 +0530

    stba: add dashboard page to all site

---
+++ 05.student-teacher-booking-appointment/apps/teacher/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return <>
+    <div className="h-full flex items-center justify-center text-gray-500">
+      <p>Select the Dashboard👋</p>
+    </div>
+  </>
+};
+
+export default Page;
