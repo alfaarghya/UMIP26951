bc23320ac7288c2f70305d3307aa2c3ffed119ad
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-21 10:59:15 +0530

    stba: update and fix sidebar

--- 05.student-teacher-booking-appointment/packages/ui/src/Sidebar.tsx
+++ 05.student-teacher-booking-appointment/packages/ui/src/Sidebar.tsx
@@ -13,37 +13,38 @@ interface SideBarProps {
 const Sidebar = ({ role }: SideBarProps) => {
   const pathname = usePathname();
 
+  //navigation button based on role
   const getNavItems = () => {
     switch (role) {
       case Role.ADMIN:
         return [
-          { label: 'Teachers', href: '/teacher', icon: 'ri-team-fill' },
-          { label: 'Students / Approved', href: '/student/approve', icon: 'ri-user-follow-fill' },
-          { label: 'Students / Pending', href: '/student/pending', icon: 'ri-user-shared-fill' },
-          { label: 'Students / Denied', href: '/student/denied', icon: 'ri-user-forbid-fill' },
+          { label: 'Teachers', href: '/dashboard/teacher', icon: 'ri-team-fill' },
+          { label: 'Students / Approved', href: '/dashboard/student/approved', icon: 'ri-user-follow-fill' },
+          { label: 'Students / Pending', href: '/dashboard/student/pending', icon: 'ri-user-shared-fill' },
+          { label: 'Students / Denied', href: '/dashboard/student/denied', icon: 'ri-user-forbid-fill' },
         ];
       case Role.TEACHER:
         return [
-          { label: 'Appointments / Pending', href: '/appointment/pending', icon: 'ri-time-line' },
-          { label: 'Appointments / Cancelled', href: '/appointment/cancelled', icon: 'ri-close-circle-line' },
-          { label: 'Appointments / Approved', href: '/appointment/approved', icon: 'ri-checkbox-circle-line' },
+          { label: 'Appointments / Approved', href: '/dashboard/appointment/approved', icon: 'ri-checkbox-circle-line' },
+          { label: 'Appointments / Pending', href: '/dashboard/appointment/pending', icon: 'ri-time-line' },
+          { label: 'Appointments / Cancelled', href: '/dashboard/appointment/cancelled', icon: 'ri-close-circle-line' },
         ];
       case Role.STUDENT:
         return [
-          { label: 'Book Appointment', href: '/appointment/teacher', icon: 'ri-calendar-check-line' },
-          { label: 'Your Appointments', href: '/appointment/all', icon: 'ri-calendar-line' },
+          { label: 'Book Appointment', href: '/dashboard/appointment/create', icon: 'ri-calendar-check-line' },
+          { label: 'Your Appointments', href: '/dashboard/appointment', icon: 'ri-calendar-line' },
         ];
       default:
         return [];
     }
   };
-
   const navItems = getNavItems();
 
   return (
     <aside className="w-64 bg-gray-900 text-white p-4 space-y-4 h-screen">
       <h2 className="text-xl font-semibold mb-4">{role} Dashboard</h2>
       <nav className="space-y-2">
+        {/* nav items on sidebar */}
         {navItems.map((item) => (
           <Link
             key={item.href}
