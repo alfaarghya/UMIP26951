4e822c0f7ea8eeae02c26519a96ae6f0c1277406
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-22 09:50:10 +0530

    blog-web: basic server setup

---
+++ 06.blog-web/apps/server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import checkRoutes from "./middleware/checkRoutes";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser()); //parse the cookies
+app.use(checkRoutes); // check all routes 
+
+
+// Default Route
+app.get("/", (_req, res) => {
+  res.status(200).json({ msg: "Hello from blog-web server" });
+});
+
+
+export default app;
