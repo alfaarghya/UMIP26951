aa6f3136c49b088a50269c16a94af2a0ac2e568c
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-22 10:03:00 +0530

    blog-web: connect all routes

--- 06.blog-web/apps/server/src/app.ts
+++ 06.blog-web/apps/server/src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import checkRoutes from "./middleware/checkRoutes";
+import authRoutes from "./routes/auth.route";
+import blogRoutes from "./routes/blog.route";
+import commentRoutes from "./routes/comment.route";
 
 const app = express();
 
@@ -11,6 +14,9 @@ app.use(express.json());
 app.use(cookieParser()); //parse the cookies
 app.use(checkRoutes); // check all routes 
 
+app.use("/api/auth", authRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/comment", commentRoutes);
 
 // Default Route
 app.get("/", (_req, res) => {
