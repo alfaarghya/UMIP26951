e0b726aa0801d7a777ca92e8c2a241f4a16806ba
alfaarghya <alfaarghya.dev@gmail.com>
2025-04-23 07:00:23 +0530

    blog-web: client side types

---
+++ 06.blog-web/packages/types/src/client/index.ts
@@ -0,0 +1,62 @@
+import { ChangeEvent } from "react";
+import { SigninSchema, SignupSchema } from "../server";
+import z from "zod";
+
+export type SignupType = z.infer<typeof SignupSchema>;
+
+export type SigninType = z.infer<typeof SigninSchema>;
+
+export interface LabelledInputType {
+  type?: string;
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface BlogCardType {
+  username: string;
+  id: string;
+  title: string;
+  content: string;
+}
+
+export interface BlogType {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string
+  author: {
+    username: string;
+  };
+}
+
+export const createBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export const updateBlogInput = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
+  id: z.string(),
+});
+
+export type CreatePostType = z.infer<typeof createBlogInput>;
+export type UpdatePostType = z.infer<typeof updateBlogInput>;
+
+export type CommentType = {
+  id: string;
+  userId: string;
+  blogId: string;
+  comment: string;
+  createdAt: string;
+  user: {
+    username: string;
+  };
+};
+
+export interface updateBlogType {
+  id: string;
+  title: string;
+  content: string;
+}
